f92da004ee170befde42f859944a4538fe6be2b4
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-04-27 20:20:35 +0100

    loadfragment2 C file

---
+++ src/main/loadfragment2.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/loadfragment2/DoRelocation.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/loadfragment2/func_8008169C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/main/loadfragment2/func_80081744.s")
