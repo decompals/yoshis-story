a1d6fc8dd445b668afce2f764f8d043886e6814b
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-06-30 01:47:31 +0100

    Decomp almost all of 80C50

--- include/global.h
+++ include/global.h
@@ -3,15 +3,27 @@
 
 #include "ultratypes.h"
 
+
+typedef struct {
+    /* 0x0 */ f32 x;
+    /* 0x4 */ f32 y;
+    /* 0x8 */ f32 z;
+} Vec3f;
+
+
+
 // Libultra
+
 f32 sinf(f32 x);
 f32 cosf(f32 x);
 
 // mtxuty-cvt
+
 void mtxcvt_F2L(Mtx* mtx, MtxF* mf);
 void mtxcvt_L2F(MtxF* mtx, Mtx* mf);
 
 // 80C50
+
 void func_800800CC(MtxF* mfA, MtxF* mfB);
 void func_80080914(MtxF* mf, f32, f32, f32, f32);
 void func_80080824(MtxF* mf, f32, f32, f32);
