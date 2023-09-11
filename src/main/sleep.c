1b968196b8fea6a8aa0bb909fe0651fa549e50d5
Derek Hensley <hensley.derek58@gmail.com>
2023-09-11 07:18:02 -0700

    sleep OK

---
+++ src/main/sleep.c
@@ -0,0 +1,13 @@
+#include "global.h"
+#include "macros.h"
+
+
+void csleep(OSTime t) {
+    OSMesgQueue mq;
+    OSMesg msg[1];
+    OSTimer timer;
+
+    osCreateMesgQueue(&mq, msg, ARRAY_COUNT(msg));
+    osSetTimer(&timer, t, 0, &mq, NULL);
+    osRecvMesg(&mq, NULL, OS_MESG_BLOCK);
+}
