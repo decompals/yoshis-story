fcf4fd3e95c9e0005f8998fd6819cdafe8406c7f
Derek Hensley <hensley.derek58@gmail.com>
2023-09-11 22:14:17 -0700

    aprintf OK

---
+++ include/aprintf.h
@@ -0,0 +1,12 @@
+#ifndef APRINTF_H
+#define APRINTF_H
+
+#include "ultra64.h"
+#include "lib/ultralib/src/libc/xstdio.h"
+
+typedef void* (*PrintCallback)(void*, const char*, size_t);
+
+s32 vaprintf(PrintCallback* func, const char* fmt, va_list ap);
+s32 aprintf(PrintCallback* func, const char* fmt, ...);
+
+#endif
