7455914026a941e6ad417a6669ea9d7c4164c625
Derek Hensley <hensley.derek58@gmail.com>
2023-10-21 09:43:06 -0700

    diff fix us (#32)

--- diff_settings.py
+++ diff_settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def apply(config, args):
-    config['baseimg'] = 'baserom.z64'
+    config['baseimg'] = 'baserom.us.z64'
     config['myimg'] = 'build/yoshisstory.us.z64'
     config['mapfile'] = 'build/yoshisstory.us.map'
     config['source_directories'] = ['./src','./include']
