d51dc308fb58879d6e42c02dca2034bf5ecc66da
Derek Hensley <hensley.derek58@gmail.com>
2024-02-16 16:28:00 -0800

    Start eepmgr

---
+++ src/main/eepmgr.c
@@ -0,0 +1,30 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007CDA0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007CDCC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007CDFC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007CF50.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007CFB0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007D0B0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007D1EC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007D2C4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007D380.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007D488.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007D508.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007D540.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007D590.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/D_800B2AE0.s")
