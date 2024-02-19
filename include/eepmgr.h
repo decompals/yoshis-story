d80c01200af23f3ff3264dbf6867358ae0f3b033
Derek Hensley <hensley.derek58@gmail.com>
2024-02-18 19:49:37 -0800

    eepmgr_HandleWrite + docs

--- include/eepmgr.h
+++ include/eepmgr.h
@@ -3,15 +3,11 @@
 
 #include "ultra64.h"
 
-typedef struct EepRequest {
-    /* 0x0 */ s16 unk0;
-    /* 0x4 */ void* unk4;
-} EepRequest;
 
-typedef struct EepRequest2 {
-    /* 0x0 */ s32 unk0;
-    /* 0x4 */ s32 unk4;
-} EepRequest2;
+typedef struct EepRequest {
+    /* 0x0 */ s16 type;
+    /* 0x4 */ u64* buffer;
+} EepRequest;
 
 typedef struct EepMgr {
     /* 0x000 */ OSMesg unk000[4];
@@ -23,11 +19,11 @@ typedef struct EepMgr {
     /* 0x050 */ char unk_04C[0x68 - 0x50];
     /* 0x068 */ OSThread thread;
     /* 0x218 */ s32 unk218;
-    /* 0x21C */ u8 unk21C;
-    /* 0x21E */ u16 unk21E;
-    /* 0x220 */ EepRequest2* unk220;
-    /* 0x224 */ u8 unk224;
-    /* 0x225 */ u8 unk225;
+    /* 0x21C */ u8 type;
+    /* 0x21E */ u16 numBlocks;
+    /* 0x220 */ u64* buffer;
+    /* 0x224 */ u8 hasData;
+    /* 0x225 */ u8 operation;
 } EepMgr; // size = 0x228
 
 #endif
--- src/main/eepmgr.c
+++ src/main/eepmgr.c
@@ -7,9 +7,13 @@
     while (true) \
     (void)0
 
-s32 D_800A9A20 = 1;
-u16 D_800A9A24[] = { 0, 0x200 / 8, 0x800 / 8 };
+s32 gEepmgrLogSeverity = 1;
+static u16 sMaxBlocks[] = { 0, EEPROM_MAXBLOCKS, EEP16K_MAXBLOCKS };
 
+#define EEP_READ_MSG 8
+#define EEP_WRITE_MSG 9
+
+//! TODO: Probably a libultra macro 
 #define OS_CYCLES_TO_USEC_ALT(c) (((u64)(c) * (1000000LL / 15625LL)) / (osClockRate / 15625LL))
 
 void func_8007CDA0(EepMgr* eepmgr) {
@@ -21,65 +25,65 @@ void func_8007CDCC(EepMgr* eepmgr) {
     eepmgr->unk044 = NULL;
 }
 
-void func_8007CDFC(EepMgr* eepmgr) {
-    s32 status;
+void eepmgr_Probe(EepMgr* eepmgr) {
+    s32 type;
     OSTime before;
     OSTime after;
 
     func_8007CDA0(eepmgr);
 
-    eepmgr->unk225 = 1;
+    eepmgr->operation = 1;
     before = osGetTime();
-    status = osEepromProbe(eepmgr->unk044);
+    type = osEepromProbe(eepmgr->unk044);
     after = osGetTime();
-    eepmgr->unk225 = 0;
+    eepmgr->operation = 0;
 
     func_8007CDCC(eepmgr);
 
-    if (D_800A9A20 >= 3) {
+    if (gEepmgrLogSeverity >= 3) {
         (void)OS_CYCLES_TO_USEC_ALT(after - before);
     }
-    if (status == 0) {
-        if (D_800A9A20 != 0) {}
-    } else if (status == 1) {
-        if (D_800A9A20 != 0) {}
-    } else if (status == 2) {
-        if (D_800A9A20 != 0) {}
+    if (type == 0) {
+        if (gEepmgrLogSeverity != 0) {}
+    } else if (type == EEPROM_TYPE_4K) {
+        if (gEepmgrLogSeverity != 0) {}
+    } else if (type == EEPROM_TYPE_16K) {
+        if (gEepmgrLogSeverity != 0) {}
     } else {
         LOOP();
     }
 
-    if (D_800A9A20 != 0) {}
+    if (gEepmgrLogSeverity != 0) {}
 
-    if (eepmgr->unk21C != status) {
+    if (eepmgr->type != type) {
         LOOP();
     }
 }
 
-void func_8007CF50(EepMgr* eepmgr, s32 arg1, s32 arg2, void* arg3) {
+void eepmgr_Setup(EepMgr* eepmgr, s32 arg1, s32 type, u64* buffer) {
     bzero(eepmgr, sizeof(EepMgr));
     eepmgr->unk218 = arg1;
-    eepmgr->unk220 = arg3;
-    eepmgr->unk21C = arg2;
-    eepmgr->unk21E = D_800A9A24[eepmgr->unk21C];
+    eepmgr->buffer = buffer;
+    eepmgr->type = type;
+    eepmgr->numBlocks = sMaxBlocks[eepmgr->type];
 }
 
-s32 func_8007CFB0(EepMgr* eepmgr, u8 arg1, EepRequest2* arg2) {
+s32 eepmgr_Read(EepMgr* eepmgr, u8 arg1, u64* buffer) {
     s32 status;
     OSTime before;
     OSTime after;
 
     func_8007CDA0(eepmgr);
 
-    eepmgr->unk225 = 1;
+    eepmgr->operation = 1;
     before = osGetTime();
-    status = osEepromRead(eepmgr->unk044, arg1, (u8*)arg2);
+    status = osEepromRead(eepmgr->unk044, arg1, (u8*)buffer);
     after = osGetTime();
-    eepmgr->unk225 = 0;
+    eepmgr->operation = 0;
 
     func_8007CDCC(eepmgr);
 
-    if (D_800A9A20 >= 2) {
+    if (gEepmgrLogSeverity >= 2) {
         (void)OS_CYCLES_TO_USEC_ALT(after - before);
     }
 
@@ -87,27 +91,27 @@ s32 func_8007CFB0(EepMgr* eepmgr, u8 arg1, EepRequest2* arg2) {
         return -1;
     }
 
-    if (D_800A9A20 != 0) {}
+    if (gEepmgrLogSeverity != 0) {}
 
     return 0;
 }
 
-s32 func_8007D0B0(EepMgr* eepmgr, u8 arg1, EepRequest2* arg2) {
+s32 eepmgr_Write(EepMgr* eepmgr, u8 addr, u64* buffer) {
     s32 status;
     OSTime before;
     OSTime after;
 
     func_8007CDA0(eepmgr);
 
-    eepmgr->unk225 = 2;
+    eepmgr->operation = 2;
     before = osGetTime();
-    status = osEepromWrite(eepmgr->unk044, arg1, (u8*)arg2);
+    status = osEepromWrite(eepmgr->unk044, addr, (u8*)buffer);
     after = osGetTime();
-    eepmgr->unk225 = 0;
+    eepmgr->operation = 0;
 
     func_8007CDCC(eepmgr);
 
-    if (D_800A9A20 >= 2) {
+    if (gEepmgrLogSeverity >= 2) {
         (void)OS_CYCLES_TO_USEC_ALT(after - before);
     }
 
@@ -115,97 +119,99 @@ s32 func_8007D0B0(EepMgr* eepmgr, u8 arg1, EepRequest2* arg2) {
         return -1;
     }
 
-    if (D_800A9A20 != 0) {}
+    if (gEepmgrLogSeverity != 0) {}
 
+    // Sleep 15ms
     csleep((15 * osClockRate) / 1000ULL);
     return 0;
 }
 
-#ifdef NON_MATCHING
-s32 func_8007D1EC(EepMgr* eepmgr, EepRequest2* arg1) {
-    EepRequest2* var_s0;
-    EepRequest2* var_s1;
-    s32 var_s2;
+s32 eepmgr_HandleWrite(EepMgr* eepmgr, u64* buffer) {
+    u64* readBuffer;
+    u64* iter;
+    s32 i;
 
-    if (eepmgr->unk21C == 0) {
-        return -1;
-    }
-    if (eepmgr->unk224 == 0) {
+    if (eepmgr->type == 0) {
         return -1;
     }
 
-    for (var_s1 = eepmgr->unk220, var_s0 = arg1, var_s2 = 0; var_s2 < eepmgr->unk21E; var_s1++, var_s0++, var_s2++) {
-        if ((var_s1->unk0 == var_s0->unk0) && (var_s1->unk4 == var_s0->unk4)) {
-            continue;
-        }
-
-        if (func_8007D0B0(eepmgr, var_s2, var_s0)) {
-            return -1;
-        }
-        var_s1->unk0 = var_s0->unk0;
-        var_s1->unk4 = var_s0->unk4;
-    }
-
-    return 0;
-}
-#else
-s32 func_8007D1EC(EepMgr* eepmgr, EepRequest2* arg1);
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/eepmgr/func_8007D1EC.s")
-#endif
-
-s32 func_8007D2C4(EepMgr* eepmgr, EepRequest2* arg1) {
-    s32 var_s0;
-    EepRequest2* req;
-
-    if (eepmgr->unk21C == 0) {
+    if (!eepmgr->hasData) {
         return -1;
     }
 
-    if (eepmgr->unk224 == 0) {
-        for (req = eepmgr->unk220, var_s0 = 0; var_s0 < eepmgr->unk21E; req++, var_s0++) {
-            if (func_8007CFB0(eepmgr, var_s0, req)) {
+    iter = eepmgr->buffer;
+    readBuffer = buffer;
+    for (i = 0; i < eepmgr->numBlocks; i++) {
+        if (*readBuffer != *iter) {
+            if (eepmgr_Write(eepmgr, i, readBuffer) != 0) {
                 return -1;
             }
+
+            *iter = *readBuffer;
         }
-        eepmgr->unk224 = 1;
+
+        iter++;
+        readBuffer++;
     }
 
-    bcopy(eepmgr->unk220, arg1, eepmgr->unk21E * 8);
     return 0;
 }
 
-void func_8007D380(void* arg) {
+s32 eepmgr_HandleRead(EepMgr* eepmgr, u64* arg1) {
+    u64* iter;
+    s32 i;
+
+    if (eepmgr->type == 0) {
+        return -1;
+    }
+
+    if (!eepmgr->hasData) {
+        iter = eepmgr->buffer;
+        for (i = 0; i < eepmgr->numBlocks; i++) {
+            if (eepmgr_Read(eepmgr, i, iter) != 0) {
+                return -1;
+            }
+            iter++;
+        }
+        eepmgr->hasData = true;
+    }
+
+    bcopy(eepmgr->buffer, arg1, eepmgr->numBlocks * EEPROM_BLOCK_SIZE);
+    return 0;
+}
+
+void eepmgr_ThreadEntry(void* arg) {
     EepRequest* req = NULL;
     EepMgr* eepmgr = (EepMgr*)arg;
     void* tmp;
 
-    func_8007CDFC(eepmgr);
+    eepmgr_Probe(eepmgr);
     do {
         osRecvMesg(&eepmgr->unk014, (OSMesg*)&req, OS_MESG_BLOCK);
-        switch (req->unk0) {
-            case 8:
+        switch (req->type) {
+            case EEP_READ_MSG:
                 if (1) {}
-                osSendMesg(&eepmgr->unk02C, (OSMesg)func_8007D2C4(eepmgr, req->unk4), OS_MESG_BLOCK);
+                osSendMesg(&eepmgr->unk02C, (OSMesg)eepmgr_HandleRead(eepmgr, req->buffer), OS_MESG_BLOCK);
                 break;
 
-            case 9:
+            case EEP_WRITE_MSG:
                 if (1) {}
-                osSendMesg(&eepmgr->unk02C, (OSMesg)func_8007D1EC(eepmgr, req->unk4), OS_MESG_BLOCK);
+                osSendMesg(&eepmgr->unk02C, (OSMesg)eepmgr_HandleWrite(eepmgr, req->buffer), OS_MESG_BLOCK);
                 break;
 
                 //! FAKE:
-                tmp = req->unk4;
+                tmp = req->buffer;
             default:
                 if (tmp != NULL) {}
         }
-    } while (req->unk0 != 4);
+    } while (req->type != 4);
 }
 
-void func_8007D488(EepMgr* eepmgr, s32 arg1, s32 arg2, void* arg3, s32 id, s32 priority, void* stack) {
-    func_8007CF50(eepmgr, arg1, arg2, arg3);
+void eepmgr_Create(EepMgr* eepmgr, s32 arg1, s32 type, u64* buffer, s32 id, s32 priority, void* stack) {
+    eepmgr_Setup(eepmgr, arg1, type, buffer);
     osCreateMesgQueue(&eepmgr->unk014, eepmgr->unk000, ARRAY_COUNT(eepmgr->unk000));
     osCreateMesgQueue(&eepmgr->unk02C, eepmgr->unk004, ARRAY_COUNT(eepmgr->unk004));
-    osCreateThread(&eepmgr->thread, id, func_8007D380, eepmgr, stack, priority);
+    osCreateThread(&eepmgr->thread, id, eepmgr_ThreadEntry, eepmgr, stack, priority);
     osStartThread(&eepmgr->thread);
 }
 
@@ -213,31 +219,31 @@ s32 func_8007D508(EepMgr* eepmgr) {
     s32 msg;
 
     osRecvMesg(&eepmgr->unk02C, (OSMesg*)&msg, OS_MESG_BLOCK);
-    eepmgr->unk048.unk0 = 0;
+    eepmgr->unk048.type = 0;
     return msg;
 }
 
-void func_8007D540(EepMgr* eepmgr, void* arg1) {
+void eepmgr_SendRead(EepMgr* eepmgr, void* buffer) {
     EepRequest* req = &eepmgr->unk048;
 
-    if (req->unk0 != 0) {
+    if (req->type != 0) {
         LOOP();
     }
 
-    req->unk0 = 8;
-    req->unk4 = arg1;
+    req->type = EEP_READ_MSG;
+    req->buffer = buffer;
     osSendMesg(&eepmgr->unk014, &eepmgr->unk048, OS_MESG_BLOCK);
 }
 
-void func_8007D590(EepMgr* eepmgr, void* arg1) {
+void eepmgr_SendWrite(EepMgr* eepmgr, void* buffer) {
     EepRequest* req = &eepmgr->unk048;
 
-    if (req->unk0 != 0) {
+    if (req->type != 0) {
         LOOP();
     }
 
-    req->unk0 = 9;
-    req->unk4 = arg1;
+    req->type = EEP_WRITE_MSG;
+    req->buffer = buffer;
     osSendMesg(&eepmgr->unk014, &eepmgr->unk048, OS_MESG_BLOCK);
 }
 
