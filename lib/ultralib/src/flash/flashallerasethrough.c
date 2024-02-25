a3476fcc3a0fc47aa0c31f3ef04d08181daf3050
Derek Hensley <hensley.derek58@gmail.com>
2024-02-24 18:09:45 -0800

    Remove ultralib

--- lib/ultralib/src/flash/flashallerasethrough.c
+++
@@ -1,8 +0,0 @@
-#include "ultra64.h"
-#include "PR/os_internal_flash.h"
-
-void osFlashAllEraseThrough(void) {
-    // start chip erase operation, no waiting for completion
-    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_CHIP_ERASE);
-    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_EXECUTE_ERASE);
-}
