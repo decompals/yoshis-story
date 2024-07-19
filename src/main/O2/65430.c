14c3e8a8e731bbeea4933b6dc87f7487f8eec53f
Derek Hensley <hensley.derek58@gmail.com>
2024-07-19 10:57:26 -0700

    65430 Split

---
+++ src/main/O2/65430.c
@@ -0,0 +1,409 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064830.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064850.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800648A4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800648DC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064920.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064AA8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064AD4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064B38.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064B64.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064BAC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064C14.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064C8C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064CB8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064D1C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064DD0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064E84.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064F38.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064F94.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80064FEC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006502C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065060.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006509C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800650B0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800650D8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006515C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800651B0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065200.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065284.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006528C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800652B4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065308.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065368.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065388.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800653CC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800653D8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065408.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065414.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065474.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065490.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800654B4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065528.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800655F4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800656BC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065728.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800657CC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006586C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800658D8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006593C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065954.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065984.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800659EC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065A54.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065ABC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065AEC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065B50.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065BC8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065C44.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065CD4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065DE4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065E4C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065F30.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80065FD0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006607C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066190.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800662B8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800663F4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066428.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066448.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066468.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800664B0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006671C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800667D4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066894.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006692C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800669C0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066A4C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066AD8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066D08.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066DE4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066E88.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066F2C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80066FA0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067114.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067170.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067284.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067390.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067400.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/bootproc.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067508.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067530.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006758C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800675D0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800675F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006767C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800676F8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800677CC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800677F8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067818.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067838.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067878.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800678B0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800678D0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800678F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067930.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067970.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067990.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800679B0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800679D0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800679F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067A10.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067A30.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067A50.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067A78.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067AAC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067ACC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067AF4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067B1C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067B3C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067B74.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067BBC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067BF8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067C30.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067C68.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067CAC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067CE4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067D28.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067D38.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067D48.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067D5C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067D94.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067DD8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067E24.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067E68.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067E78.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067E98.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067EB8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067ED8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067EF8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067F18.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067F38.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067F58.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067F78.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80067FF4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006806C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068190.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006820C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006823C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800682D4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068320.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068340.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800683A4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068418.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068438.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068470.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800684E8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068560.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800685AC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800685F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006863C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006872C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800687B8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068890.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800688CC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006899C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800689E8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800689F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068AB0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068B0C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068B2C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068BC0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068C00.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068C74.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068CD4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068CF8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068D24.s")
+
+void func_80068D8C(void) {
+}
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068D94.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068E24.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068ED4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80068F10.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069000.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069030.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006915C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006919C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006920C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800692D4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800694BC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069508.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006958C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069610.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069644.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069684.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800696D4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069728.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006976C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800697F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_8006988C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069900.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069964.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_800699BC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069A80.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069B34.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/O2/65430/func_80069C10.s")
