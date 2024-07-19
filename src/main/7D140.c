3d3bc2ccb4af3c62523193326da5050ddc67af7b
Derek Hensley <hensley.derek58@gmail.com>
2024-07-19 16:04:42 -0700

    Split 7D140

---
+++ src/main/7D140.c
@@ -0,0 +1,10 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7D140/func_8007C540.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7D140/func_8007C5EC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7D140/func_8007C61C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7D140/func_8007C640.s")
